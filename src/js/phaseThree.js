8e98676b1f9fd7582f4cc62b5882929bc4bf81e0
LucasEfdl <lucasefdl@gmail.com>
2023-03-24 15:04:50 -0300

    criação da fase 3

--- src/js/phaseThree.js
+++ src/js/phaseThree.js
@@ -1,21 +1,45 @@
 // object animation script
 const gameContainer = document.getElementById("game-container");
-const gameObject = document.getElementById("object");
-const startLevel = document.getElementById("startLevel");
-const gameObject2 = document.getElementById("object2");
-const gameObject3 = document.getElementById("object3");
+const tatu = document.getElementById("object");
+const raposa = document.getElementById("object2");
+const carro = document.getElementById("object3");
 
+//Botao começar
+const overlay = document.querySelector(".overlay");
+
+//Butons
+const startLevel = document.getElementById("startLevel");
 const startBtn = document.getElementById("startGame");
 const verifyBtn = document.getElementById("question-options");
 const resetBtn = document.getElementById("reset");
-
-// question animation script
-const text = document.getElementById("question-text");
 const startButton = document.getElementById("start-button");
+const startButton2 = document.getElementById("start_button_2");
+
+//COntrolam o tempo
+var FPS = 60;
+var dt = 100; // ms
+var t = 0;
+
+//Variaveis do tatu
+var positionInitialTatu = 0;
+var positionTatu = positionInitialTatu;
+var VelocityTatu = 20;
+
+//Variaveis da raposa
+var positionInitialRaposa = 0;
+var positionRaposa = positionInitialRaposa;
+var VelocityRaposa = 40;
+
+//Variaveis do carro
+var positionInitialCarro = 0;
+var positionCarro = positionInitialCarro;
+var VelocityCarro = 30;
 
 class Game {
-  constructor(object) {
-    this.object = object;
+  constructor(tatu, raposa, carro) {
+    this.tatu = tatu;
+    this.raposa = raposa
+    this.carro = carro
   }
 
   displayGameContainer() {
@@ -23,55 +47,79 @@ class Game {
     document.getElementById("game-container").style.display = "block";
   }
 
-  start() {
-    const obj = this.object;
-    var FPS = 60;
-    var dt = 100; // ms
-    var xo = 0;
-    var t = 0; // tempo
-    var posX = xo;
-    var vox = 40;
-    var tempo = 0;
-
-    const interval = setInterval(() => {
-      Math.round((tempo += 0.87));
-      document.getElementById("tempo").value = tempo.toFixed(0) + " min";
+  start_1() {
+    const momento_1 = setInterval(() => {
       t = t + dt / 1000;
-      posX = xo + vox * t;
-      obj.style.left = `${posX}px`;
-      if (posX >= gameContainer.offsetWidth / 2 - 200) {
-        console.log(posX);
-        clearInterval(interval);
-        gameObject2.style.display = "block";
-        gameObject3.style.display = "block";
-        overlay2.style.display = "none";
+      positionTatu = positionInitialTatu + VelocityTatu * t;
+      this.tatu.style.left = `${positionTatu}px`;
+      if (positionTatu >= gameContainer.offsetWidth / 2 - 240) {
+        clearInterval(momento_1);
+        raposa.style.display = "block";
+        carro.style.display = "block";
+        startButton2.style.display = "block";
+      }
+    }, 1000 / FPS);
+  }
+
+  start_2() {
+    t = 0
+    positionInitialTatu = gameContainer.offsetWidth / 2 - 240;
+    positionInitialRaposa = 0
+    positionInitialCarro = 0
+    const momento_2 = setInterval(() => {
+      
+      t = t + dt / 1000;
+      positionTatu = positionInitialTatu + VelocityTatu * t;
+      positionRaposa = positionInitialRaposa + VelocityRaposa * t;
+      positionCarro= positionInitialCarro + VelocityCarro * t;
+      
+      this.tatu.style.left = `${positionTatu}px`;
+      this.raposa.style.left = `${positionRaposa}px`;
+      this.carro.style.top = `${positionCarro}px`;
+      if (positionTatu >= gameContainer.offsetWidth - 200) {
+        clearInterval(momento_2);
+        alert("O tatu escapou com sucesso!!");
+      } else if(positionRaposa >= positionTatu){
+        clearInterval(momento_2)
+        alert("O peba foi pego pela raposa!!")
       }
     }, 1000 / FPS);
   }
 
   reset() {
-    const obj = this.object;
-    obj.style.left = 0 + "px";
-    document.getElementById("tempo").value = "";
+    //resetando as posições
+    tatu.style.left = 0 + "px";
+    raposa.style.left = 0 + "px";
+    carro.style.top = 0 + "px";
+
+    //display none
+    raposa.style.display = "none";
+    carro.style.display = "none";
+
+
+    
   }
 }
 
-const game = new Game(gameObject);
-const overlay = document.querySelector(".overlay");
-const overlay2 = document.querySelector(".overlay2");
+const game = new Game(tatu, raposa, carro);
 
 startButton.addEventListener("click", () => {
   game.displayGameContainer();
-  overlay.classList.add("active");
+  overlay.classList.add("active"); // ajeitar essa merda
   startLevel.addEventListener("click", () => {
-    game.start();
+    game.start_1();
     overlay.style.display = "none";
   });
 });
 
+startButton2.addEventListener("click", () => {
+  startButton2.style.display = "none"
+  game.start_2();
+})
+
 resetBtn.addEventListener("click", () => {
   game.reset();
   setTimeout(() => {
-    game.start();
+    game.start_1();
   }, 3000);
 });
