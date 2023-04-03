7f0f75be6766874a81fd0aeffbc00d2aa40ef88d
TIHeitorDS <heitorclaudinods@gmail.com>
2023-04-03 12:55:06 -0300

    atualização da fase 1

--- src/js/phaseOne.js
+++ src/js/phaseOne.js
@@ -1,31 +1,37 @@
-// object animation script
+// **********************  GENERAL  ********************** //
 const gameContainer = document.getElementById("game-container");
-const gameObject = document.getElementById("object");
-const gameObject2 = document.getElementById("object2");
-const startBtn = document.getElementById("startGame");
-const verifyBtn = document.getElementById("question-options");
-const resetBtn = document.getElementById("reset");
-const startLevel = document.querySelector("[data-js=startLevel]");
-var FPS = 60;
-var dt = 100; // ms
-var xo = 500;
-var t = 0; // tempo
-var posX = xo;
-var vox2 = 150;
-var xo2 = 0;
-var posX2 = xo2;
-
-// question animation script
 const text = document.getElementById("question-text");
-const startButton = document.getElementById("start-button");
-
-// overlay script
 const overlay = document.querySelector("[data-js=overlay]");
+const inputValocity = document.getElementById("velocidadeDigitada");
 
+var answer;
+
+// **********************  OBJECTS  ********************** //
+const mainObject = document.getElementById("object");
+const secondObject = document.getElementById("object2");
+
+// **********************  BUTTONS  ********************** //
+const intoGame = document.getElementById("start-button");
+const confirmAnswert = document.querySelector("[data-js=confirmAnswert]");
+const resetBtn = document.getElementById("reset");
+
+// **********************  EQUATIONS  ********************** //
+var FPS = 60;
+var mainPosition = 0;
+var secondPosition = 0;
+var mainVelocity = 90; // main object
+var secondVelocity = 60; // second object
+var distance = 100; // ms
+
+var xo = 0;
+var xo2 = -500;
+var time = 0;
+
+// **********************  THE GAME  ********************** //
 class Game {
-  constructor(object) {
-    this.object = object;
-    this.object2 = object2;
+  constructor(mObject, sObject) {
+    this.object = mObject;
+    this.object2 = sObject;
   }
 
   displayGameContainer() {
@@ -39,42 +45,70 @@ class Game {
   }
 
   start() {
-    var vox = document.getElementById("velocidadeDigitada").value;
     const interval = setInterval(() => {
-      t = t + dt / 1000;
-      posX = xo + vox * t;
-      posX2 = xo2 + vox2 * t;
-      this.object.style.left = `${posX}px`;
-      this.object2.style.left = `${posX2}px`;
-      if (posX > gameContainer.offsetWidth - 100) {
-        posX = xo;
-        t = 0;
+      this.object2.classList.replace("d-none", "d-block");
+
+      time += distance / 1000;
+      mainPosition = xo + mainVelocity * time;
+
+      secondPosition = xo2 + secondVelocity * time;
+
+      this.object.style.left = `${mainPosition}px`;
+      this.object2.style.left = `${secondPosition}px`;
+      xo += 5;
+      xo2 += 10;
+
+      if (Math.floor(mainPosition) == 499) {
         clearInterval(interval);
+        overlay.classList.replace("d-none", "d-block");
+        confirmAnswert.addEventListener("click", () => {
+          answer = Number(inputValocity.value);
+          mainVelocity = answer;
+          overlay.classList.replace("d-block", "d-none");
+          this.continue();
+        });
       }
-      if (posX2 >= posX - 100) {
+
+      if (this.object2.offsetLeft > this.object.offsetLeft - 100) {
+        alert("você perdeu o jogo!");
+        clearInterval(interval);
+      } else if (mainPosition > gameContainer.offsetWidth - 100) {
+        alert("você ganhou o jogo!");
         clearInterval(interval);
       }
     }, 1000 / FPS);
   }
 
+  continue() {
+    this.start();
+  }
+
   reset() {
-    this.object.style.left = 500 + "px";
-    this.object2.style.left = 0 + "px";
+    time = 0;
+    xo = 0;
+    xo2 = -500;
+    mainPosition = 0;
+    mainVelocity = 90;
+    secondPosition = 0;
+
+    this.object.style.left = 0 + "px";
+    this.object2.style.left = -500 + "px";
   }
 }
 
-const game = new Game(gameObject);
+const game = new Game(mainObject, secondObject);
 
-startButton.addEventListener("click", () => {
+intoGame.addEventListener("click", () => {
   game.displayGameContainer();
-  startLevel.addEventListener("click", () => {
+
+  setTimeout(() => {
     game.start();
-    overlay.classList.replace("d-block", "d-none");
-  });
+  }, 3000);
 });
 
 resetBtn.addEventListener("click", () => {
   game.reset();
+
   setTimeout(() => {
     game.start();
   }, 3000);
