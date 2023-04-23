d0e2ab4faa962d2cd57b7b26d015e76a11e6c1ee
TIHeitorDS <heitorclaudinods@gmail.com>
2023-04-22 21:14:23 -0300

    alterção no visual

--- src/js/phase_two.js
+++ src/js/phase_two.js
@@ -1,166 +1,58 @@
-// Object animation script
-const gameContainer = document.getElementById("game-container");
-const tatu = document.getElementById("object");
-const raposa = document.getElementById("object2");
-const carro = document.getElementById("object3");
+// **********************  GENERAL  ********************** //
+const initialScreen = document.getElementById("initial-screen");
+const gameScreen = document.getElementById("game-screen");
+const overlay = document.querySelector("[data-js=overlay]");
 
-//Botao começar
-const overlay = document.querySelector(".overlay");
+// **********************  OBJECTS  ********************** //
+const armadillo = document.getElementById("object");
+const fox = document.getElementById("object2");
+const car = document.getElementById("object3");
 
-//Butons
-const startLevel = document.getElementById("startLevel");
-const startBtn = document.getElementById("startGame");
-const verifyBtn = document.getElementById("question-options");
-const resetBtn = document.getElementById("reset");
+// **********************  BUTTONS  ********************** //
 const startButton = document.getElementById("start-button");
-const startButton2 = document.getElementById("start_button_2");
 
-//Controlam o tempo
-var FPS = 60;
-var dt = 100; // ms
-var t = 0;
-
-//Variaveis do tatu
-var positionInitialTatu = 0;
-var positionTatu = positionInitialTatu;
-var VelocityTatu = 20;
-var VelocityTatu2;
-
-//Variaveis da raposa
-var positionInitialRaposa = 0;
-var positionRaposa = positionInitialRaposa;
-var VelocityRaposa = 53;
-
-//Variaveis do carro
-var positionInitialCarro = 0;
-var positionCarro = positionInitialCarro;
-var VelocityCarro = 80;
-var resposta;
+// **********************  POSITION TIME FUNCTION VAR  ********************** //
+let positions = [0, 0];
+const initialPositions = [0, -450];
+const velocities = [21, 40];
+let time = 0;
 
 class Game {
-  constructor(tatu, raposa, carro) {
-    this.tatu = tatu;
-    this.raposa = raposa;
-    this.carro = carro;
+  constructor(armadillo, fox) {
+    this.armadillo = armadillo;
+    this.fox = fox;
   }
 
-  displayGameContainer() {
-    document.getElementById("game-start").style.display = "none";
-    document.getElementById("game-container").style.display = "block";
+  showGameContainer() {
+    initialScreen.classList.replace("d-flex", "d-none");
+    gameScreen.classList.replace("d-none", "d-flex");
   }
 
-  start_1() {
-    const momento_1 = setInterval(() => {
-      t = t + dt / 1000;
-      positionTatu = positionInitialTatu + VelocityTatu * t;
-      this.tatu.style.left = `${positionTatu}px`;
-      if (positionTatu >= gameContainer.offsetWidth / 2 - 125) {
-        clearInterval(momento_1);
-        raposa.style.display = "block";
-        carro.style.display = "block";
-        startButton2.style.display = "block";
+  startGame() {
+    const mru = setInterval(() => {
+      positions[0] = initialPositions[0] + velocities[0] * time; // Fórmula da função horária da posição
+      positions[1] = initialPositions[1] + velocities[1] * time; // Fórmula da função horária da posição
+      this.armadillo.style.left = `${positions[0]}px`; // Movimentação do tatu
+      this.fox.style.left = `${positions[1]}px`;
+      if (time == 20) {
+        clearInterval(mru); // O objeto tem de parar na metade da width
+        setTimeout(() => {
+          overlay.classList.replace("d-none", "d-block");
+        }, 800);
       }
-    }, 1000 / FPS);
-  }
-
-  start_2() {
-    resposta = parseFloat(
-      prompt("Qual a velocidade do bega para escapa dessa situação?")
-    );
-    t = 0;
-    positionInitialTatu = gameContainer.offsetWidth / 2 - 100;
-    positionInitialRaposa = 0;
-    positionInitialCarro = 0;
-    VelocityTatu2 = resposta;
-    const momento_2 = setInterval(() => {
-      t = t + dt / 1000;
-      positionTatu = positionInitialTatu + VelocityTatu2 * t;
-      positionRaposa = positionInitialRaposa + VelocityRaposa * t;
-      positionCarro = positionInitialCarro + VelocityCarro * t;
-
-      this.tatu.style.left = `${positionTatu}px`;
-      this.raposa.style.left = `${positionRaposa}px`;
-      this.carro.style.top = `${positionCarro}px`;
-      if (positionTatu > gameContainer.offsetWidth - 60) {
-        clearInterval(momento_2);
-        alert("O tatu escapou com sucesso!!");
-      } else if (positionRaposa >= positionTatu - 60) {
-        clearInterval(momento_2);
-        alert("O peba foi pego pela raposa!!");
-      } else if (positionCarro >= gameContainer.offsetHeight - 155) {
-        carro.style.display = "none";
-      } else if (
-        tatu.offsetLeft >= 900 &&
-        tatu.offsetLeft <= 980 &&
-        carro.offsetTop >= 277 &&
-        carro.offsetTop <= 429
-      ) {
-        alert("O tatu foi atropelado!");
-        clearInterval(momento_2);
-      } else if (positionRaposa >= gameContainer.offsetWidth) {
-        raposa.style.display = "none";
+      if (time == 40) {
+        clearInterval(mru);
+        this.armadillo.style.left = `${positions[0] - 100}px`;
+        this.objectTwo.style.left = `${positions[1] - 150}px`;
       }
-    }, 1000 / FPS);
-  }
-
-  reset() {
-    //Variaveis do tatu
-    positionInitialTatu = 0;
-    positionTatu = positionInitialTatu;
-    VelocityTatu = 20;
-    VelocityTatu2 = 10;
-
-    // Variaveis da raposa
-    positionInitialRaposa = 0;
-    positionRaposa = positionInitialRaposa;
-    VelocityRaposa = 60;
-
-    //Variaveis do carro
-    positionInitialCarro = 0;
-    positionCarro = positionInitialCarro;
-    VelocityCarro = 80;
-    resposta;
-
-    //resetando as posições
-    tatu.style.left = 0 + "px";
-    raposa.style.left = 0 + "px";
-    carro.style.top = 0 + "px";
-
-    //display none
-    raposa.style.display = "none";
-    carro.style.display = "none";
+      time++;
+    }, 1000 / 60);
   }
 }
 
-const game = new Game(tatu, raposa, carro);
+const game = new Game(armadillo, fox);
 
 startButton.addEventListener("click", () => {
-  game.displayGameContainer();
-  overlay.classList.add("active"); // ajeitar essa merda
-  startLevel.addEventListener("click", () => {
-    game.start_1();
-    overlay.style.display = "none";
-  });
-});
-
-startButton2.addEventListener("click", () => {
-  startButton2.style.display = "none";
-  game.start_2();
-});
-
-//Defeituoso
-resetBtn.addEventListener("click", () => {
-  console.log(positionTatu);
-  game.reset();
-  console.log(positionTatu);
-  overlay.style.display = "block";
-  startLevel.addEventListener("click", () => {
-    overlay.style.display = "none";
-    game.start_1();
-  });
-
-  startButton2.addEventListener("click", () => {
-    startButton2.style.display = "none";
-    game.start_2();
-  });
+  game.showGameContainer();
+  game.startGame();
 });
