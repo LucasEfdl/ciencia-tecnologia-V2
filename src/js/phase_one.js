c54fdc184d69a6a6bbe223c3ee27245193762fac
TIHeitorDS <heitorclaudinods@gmail.com>
2023-05-06 22:56:01 -0300

    alguns ajustes

--- src/js/phase_one.js
+++ src/js/phase_one.js
@@ -1,115 +1,185 @@
 // **********************  GENERAL  ********************** //
 const gameContainer = document.getElementById("game-container");
-const text = document.getElementById("question-text");
 const overlay = document.querySelector("[data-js=overlay]");
-const inputValocity = document.getElementById("velocidadeDigitada");
+const userName = document.querySelector("input[for=nameUser]");
 
-var answer;
+const input = document.getElementById("inputVelocity");
 
 // **********************  OBJECTS  ********************** //
-const mainObject = document.getElementById("object");
-const secondObject = document.getElementById("object2");
+const armadillo = document.getElementById("object");
+const fox = document.getElementById("object2");
 
 // **********************  BUTTONS  ********************** //
-const intoGame = document.getElementById("start-button");
-const confirmAnswert = document.querySelector("[data-js=confirmAnswert]");
-const resetBtn = document.getElementById("reset");
+const intoGameButton = document.getElementById("start-button");
+const confirmAnswertButton = document.querySelector("[data-js=confirmAnswert]");
+const resetButton = document.getElementById("reset");
 
-// **********************  EQUATIONS  ********************** //
-var FPS = 60;
-var mainPosition = 0;
-var secondPosition = 0;
-var mainVelocity = 90; // main object
-var secondVelocity = 60; // second object
-var distance = 100; // ms
+// **********************  POSITION TIME FUNCTION VAR  ********************** //
+const initialPositions = [0, -450];
+const velocities = [30, 40];
+let time = 0;
 
-var xo = 0;
-var xo2 = -500;
-var time = 0;
+let difference = 0; // variável que guarda a diferença das posições
+let currentPosition = 0;
 
 // **********************  GAME  ********************** //
 class Game {
-  constructor(mObject, sObject) {
-    this.object = mObject;
-    this.object2 = sObject;
+  constructor(armadillo, fox) {
+    this.armadillo = armadillo;
+    this.fox = fox;
   }
 
   displayGameContainer() {
-    document.getElementById("initial-screen").style.backgroundImage = "none";
+    document
+      .getElementById("initial-screen")
+      .classList.replace("d-flex", "d-none");
     document
       .querySelector("[data-js=game-screen]")
       .classList.replace("d-none", "d-flex");
+  }
+
+  displayGameContainerStart() {
+    document
+      .getElementById("initial-screen")
+      .classList.replace("d-none", "d-flex");
     document
       .getElementById("start-button")
+      .classList.replace("d-none", "d-flex");
+    document
+      .querySelector("[data-js=game-screen]")
       .classList.replace("d-flex", "d-none");
   }
 
-  start() {
-    const interval = setInterval(() => {
-      this.object2.classList.replace("d-none", "d-block");
+  startGame() {
+    const moveObjects = () => {
+      // Calculando a posição dos objetos pela equação horária da posição
+      currentPosition = initialPositions[0] + velocities[0] * time;
 
-      time += distance / 1000;
-      mainPosition = xo + mainVelocity * time;
-
-      secondPosition = xo2 + secondVelocity * time;
-
-      this.object.style.left = `${mainPosition}px`;
-      this.object2.style.left = `${secondPosition}px`;
-      xo += 5;
-      xo2 += 10;
-
-      if (Math.floor(mainPosition) == 499) {
-        clearInterval(interval);
-        overlay.classList.replace("d-none", "d-block");
-        confirmAnswert.addEventListener("click", () => {
-          answer = Number(inputValocity.value);
-          mainVelocity = answer;
-          overlay.classList.replace("d-block", "d-none");
-          this.continue();
-        });
+      // Quando a resposta estiver errada, calcula a diferença de posição
+      if (difference > currentPosition) {
+        difference -= currentPosition;
       }
 
-      if (this.object2.offsetLeft > this.object.offsetLeft - 100) {
-        alert("você perdeu o jogo!");
-        clearInterval(interval);
-      } else if (mainPosition > gameContainer.offsetWidth - 100) {
-        alert("você ganhou o jogo!");
-        clearInterval(interval);
-      }
-    }, 1000 / FPS);
-  }
+      this.armadillo.style.left = `${currentPosition + difference}px`;
+      this.fox.style.left = `${initialPositions[1] + velocities[1] * time}px`;
 
-  continue() {
-    this.start();
+      if (time == 20) {
+        clearInterval(mru); // O objeto tem de parar na metade da width
+
+        currentPosition = parseInt(this.armadillo.style.left);
+
+        setTimeout(() => {
+          overlay.classList.replace("d-none", "d-block");
+        }, 800);
+      }
+      // Fazendo a raposa parar quando sua posição for muito próxima da posição do Tatu e a resposta do usuário estiver errada
+      if (
+        parseInt(this.fox.style.left) >
+        parseInt(this.armadillo.style.left) - 55
+      ) {
+        clearInterval(mru);
+        resetButton.disabled = false;
+      }
+      // Quando a resposta estiver certa, o jogo acaba no tempo de 40s
+      if (currentPosition >= gameContainer.offsetWidth) {
+        clearInterval(mru);
+        this.armadillo.style.left = "1100px";
+        this.fox.style.left = "1000px";
+        resetButton.disabled = false;
+      }
+
+      console.log(currentPosition);
+      time++;
+    };
+    const mru = setInterval(moveObjects, 1000 / 60);
   }
 
   reset() {
+    velocities[0] = 30;
+    velocities[1] = 40;
     time = 0;
-    xo = 0;
-    xo2 = -500;
-    mainPosition = 0;
-    mainVelocity = 90;
-    secondPosition = 0;
 
-    this.object.style.left = 0 + "px";
-    this.object2.style.left = -500 + "px";
+    this.armadillo.style.left = `${initialPositions[0]}px`;
+    this.fox.style.left = `${initialPositions[1]}px`;
+
+    input.value = "";
+
+    difference = 0;
+    currentPosition = 0;
+
+    resetButton.disabled = true;
+    confirmAnswertButton.disabled = true;
   }
 }
 
-const game = new Game(mainObject, secondObject);
+const game = new Game(armadillo, fox);
 
-intoGame.addEventListener("click", () => {
+intoGameButton.addEventListener("click", () => {
   game.displayGameContainer();
-
   setTimeout(() => {
-    game.start();
-  }, 3000);
+    game.startGame();
+  }, 1000);
 });
 
-resetBtn.addEventListener("click", () => {
+input.addEventListener("input", (e) => {
+  let numValue = parseInt(e.target.value);
+  if (e.target.value != "" && !isNaN(numValue)) {
+    confirmAnswertButton.disabled = false;
+  } else {
+    confirmAnswertButton.disabled = true;
+  }
+});
+
+confirmAnswertButton.addEventListener("click", () => {
+  overlay.classList.replace("d-block", "d-none");
+  let num = 0;
+  const selectInputValue = input.value;
+  const numValue = selectInputValue;
+  num = parseInt(numValue);
+  if (num < 30) {
+    velocities[0] = num;
+    difference = currentPosition + velocities[0];
+    game.startGame();
+    setTimeout(() => {
+      var modalElement = document.getElementById("meuModal2");
+      var modal = new bootstrap.Modal(modalElement);
+      modal.show();
+    }, 1500);
+  } else {
+    velocities[0] = num;
+    game.startGame();
+    setTimeout(() => {
+      var modalElement = document.getElementById("meuModal");
+      var modal = new bootstrap.Modal(modalElement);
+      modal.show();
+    }, 1500);
+  }
+});
+
+resetButton.addEventListener("click", () => {
   game.reset();
-
   setTimeout(() => {
-    game.start();
+    game.startGame();
   }, 3000);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var botaoReset = document.getElementById("btreset");
+  botaoReset.addEventListener("click", resetGame);
+});
+
+function resetGame() {
+  game.reset();
+  setTimeout(() => {
+    game.startGame();
+  }, 3000);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  var botaoReset = document.getElementById("btGo");
+  botaoReset.addEventListener("click", nextGame);
+});
+
+function nextGame() {
+  window.location.href = "phase_two.html";
+}
