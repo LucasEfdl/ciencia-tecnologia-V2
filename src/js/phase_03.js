5c2be7140dcec234fb5d5d64c88c076f7874d188
LucasEfdl <lucasefdl@gmail.com>
2023-11-11 19:57:29 -0300

    phase two complete

--- src/js/phase_03.js
+++ src/js/phase_03.js
@@ -19,9 +19,6 @@ const questionModalElement = document.getElementById(`question${breakpoint}`);
 const nextPhaseModalElement = document.getElementById(
   `next-phase-modal${breakpoint}`
 );
-const gameOverModalElement = document.getElementById(
-  `game-over-modal${breakpoint}`
-);
 const options = document.querySelectorAll('input[type="radio"][name="option"]');
 const showQuestionButton = document.querySelector(
   `[data-show-question${breakpoint}]`
@@ -34,7 +31,6 @@ const progressLose = document.querySelector("[data-progress-lose]");
 const timeOverModal = new bootstrap.Modal(timeOverModalElement);
 const questionModal = new bootstrap.Modal(questionModalElement);
 const nextPhaseModal = new bootstrap.Modal(nextPhaseModalElement);
-const gameOverModal = new bootstrap.Modal(gameOverModalElement);
 let [milliseconds, seconds, minutes] = [0, 0, 3];
 let [elapsedMinutes, elapsedSeconds, elapsedMilliseconds] = [0, 0, 0];
 let [minutesSpent, secondsSpent] = [0, 0];
@@ -158,11 +154,12 @@ submitAnswerButton.addEventListener("click", () => {
     nextPhaseModal.show();
     questionModal.hide();
     progressWin.style.width = "100%";
-    velocity = `velocidade = ${answer} (certo)`;
+    velocity = `velocidade = ${answer} (Resposta Certa)`;
   } else {
-    gameOverModal.show();
+    nextPhaseModal.show();
     questionModal.hide();
     showQuestionButton.disabled = false;
+    velocity = `velocidade = ${answer} (Resposta Errada)`;
   }
 
   localStorage.setItem("question-03", velocity);
